04b81a33fa6ca879f4ea6c7b819f534f5f4297d0
resolut <samdimus@gmail.com>
2018-03-28 16:38:20 +0300

    добавлено решение задания 12.4

---
+++ ex12/ex12-4/js/app.js
@@ -0,0 +1,15 @@
+var login = prompt("Кто пришел?");
+if (login == null) {
+    alert("Вход отменен");
+} else if (login === "Админ") {
+    var password = prompt("Пароль?");
+    if (password == null) {
+        alert("Вход отменен");
+    } else if (password === "Черный Властелин") {
+        alert("Добро пожаловать!");
+    } else {
+        alert("Пароль неверен");
+    }
+} else {
+    alert("Я вас не знаю");
+}
