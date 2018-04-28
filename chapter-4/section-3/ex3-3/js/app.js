1b985bdfa41e5c8a76d47e9798d924265befe56d
Dmitriy Samofalov <samdimus@gmail.com>
2018-04-28 18:05:17 +0300

    добавлено решение задачи 3-3

--- chapter-4/section-3/ex3-3/js/app.js
+++ chapter-4/section-3/ex3-3/js/app.js
@@ -1,6 +1,9 @@
-function ucFirst(str) {
-    return str[0].toUpperCase() + str.slice(1);
+function truncate(str, maxlength) {
+    if (str.length > maxlength) {
+        return str.slice(0, maxlength - 3) + "...";
+    } 
+    return str;
 }
 
-console.log(ucFirst("dima"));
-console.log(ucFirst("ksysha"));
+console.log(truncate("Вот, что мне хотелось бы сказать на эту тему:" , 20));
+console.log(truncate("Всем привет!", 10));
